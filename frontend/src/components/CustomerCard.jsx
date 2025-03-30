bc0c6ee2814e8f788d3ce007c3f2dcef6b51ec1e
Zheng Jie Koay <koayzhengjie2@gmail.com>
2025-03-30 19:07:00 +1000

    edited ci.16

--- frontend/src/components/CustomerCard.jsx
+++ frontend/src/components/CustomerCard.jsx
@@ -1,17 +1,14 @@
 import React from'react';
-import { Card } from 'react-bootstrap';
 
 const CustomerCard = ({ customer }) => {
     return(
-        <Card classname="mb-3">
-            <Card.Body>
-                <Card.Title>{customer.name}</Card.Title>
-                <Card.Text>
-                    <strong>Email:</strong> {customer.email}<br />
-                    <strong>Phone:</strong> {customer.phone}
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <div classname="mb-3">
+            <div className="card-body">
+                <h2 className="card-title">{customer.name}</h2>
+                <p className="card-text">{customer.email}</p>
+                <p className="card-text">{customer.phone}</p>
+            </div>
+        </div>
     );
 };
 
